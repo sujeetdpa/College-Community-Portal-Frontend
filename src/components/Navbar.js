2a7d42afb63e1f5986768d2d0c4569f03a0cc04f
Sujeet Mourya <sujeetdpa@gmail.com>
2022-04-13 10:00:44 +0530

    [implemented] fetching image from server

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,6 +4,11 @@ import { Link } from 'react-router-dom'
 import './Navbar.css'
 
 export default function Navbar() {
+    const [accessToken,setAccessToken]=useState(localStorage.getItem("access_token"));
+
+    useEffect(()=>{
+        setAccessToken(localStorage.getItem("access_token"));
+    },[accessToken])
     return (
         <div>
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
@@ -14,7 +19,7 @@ export default function Navbar() {
                     </button>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                            {/* {(accessToken === undefined || accessToken === null) ? */}
+                            {(accessToken === undefined || accessToken === null) ?
                                 <>
                                     <li className="nav-item">
                                         <Link className="nav-link" to="/login">Login</Link>
@@ -33,7 +38,7 @@ export default function Navbar() {
                                     <li className="nav-item">
                                         <Link className="nav-link" to="/logout">Logout</Link>
                                     </li>
-                                </>
+                                </>}
                         </ul>
                         <form className="d-flex">
                             <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
--- src/components/Post.js
+++ src/components/Post.js
@@ -1,30 +1,7 @@
 import React from 'react'
 import './Post.css'
 
-export default function Post({postData}) {
-  const getImage=(imageId)=>{
-    async function fetchImage(){
-      const authHeader="Bearer "+localStorage.getItem("access_token");
-      const options={
-        method: 'GET',
-        headers:{
-            'Authorization': authHeader
-        }
-      }
-      const response=await fetch("http://localhost:8080/api/post/local/storage/download/image/"+imageId,options);
-      const blo= await response.blob();
-      reader.readAsDataURL(blo);
-    }
-    const reader=new FileReader();
-    let image;
-    reader.onloadend = () => {
-      const base64data = reader.result; 
-      image=base64data;               
-      console.log(base64data);
-    }
-    return image;
-  }
-
+export default function Post({ postData }) {
   return (
     <div>
       <div className="container mt-5 mb-5">
@@ -36,43 +13,28 @@ export default function Post({postData}) {
                   <div className="d-flex flex-column ml-2"> <span className="font-weight-bold">{postData.fullName}</span> <small className="text-primary">Collegues</small> </div>
                 </div>
                 <div className="d-flex flex-row mt-1 ellipsis"> <small className="mr-2">{postData.creationDate}</small> <i className="fa fa-ellipsis-h"></i> </div>
-              </div> 
+              </div>
               <div className="p-2">
                 <h5>{postData.title}</h5>
                 <p className="text-justify">{postData.description}</p>
-                </div>
-              {postData.imageIds.length>0 ? <img src={getImage(postData.imageIds[0])} className="img-fluid" alt='dsvv'/>:""}
+              </div>
+              {postData.imageIds.length > 0 ? <img src={"http://localhost:8080/api/post/local/storage/download/image/" + postData.imageIds[0]} className="img-fluid" alt='dsvv' /> : ""}
               <div className="p-2">
-                <hr/>
-                  <div className="d-flex justify-content-between align-items-center">
-                    <div className="d-flex flex-row icons d-flex align-items-center">
-                      <i className="fa-regular fa-thumbs-up"> </i> {postData.noOfLikes}
-                      <i className="fa-regular fa-thumbs-down"></i> 
-                      </div>
-                    <div className="d-flex flex-row muted-color"> <span>{postData.noOfComments} Comments</span>
+                <hr />
+                <div className="d-flex justify-content-between align-items-center">
+                  <div className="d-flex flex-row icons d-flex align-items-center">
+                    <h4 className="bi bi-hand-thumbs-up px-2">{postData.noOfLikes}</h4> 
+                    <h4 className="bi bi-hand-thumbs-down px-3"></h4>
                   </div>
-                <hr/>
-                <div className="comments">
-                  <div className="d-flex flex-row mb-2"> <img src="https://i.imgur.com/9AZ2QX1.jpg" width="40" className="rounded-image" alt='dv' />
-                    <div className="d-flex flex-column ml-2"> <span className="name">Daniel Frozer</span> <small className="comment-text">I like this alot! thanks alot</small>
-                      <div className="d-flex flex-row align-items-center status"> <small>Like</small> <small>Reply</small> <small>Translate</small> <small>18 mins</small> </div>
-                    </div>
+                  <div className="d-flex flex-row muted-color"><h4 className="bi bi-chat-left-text px-3"> {postData.noOfComments}</h4>
                   </div>
-                  <div className="d-flex flex-row mb-2"> <img src="https://i.imgur.com/1YrCKa1.jpg" width="40" className="rounded-image" alt='ds' />
-                    <div className="d-flex flex-column ml-2"> <span className="name">Elizabeth goodmen</span> <small className="comment-text">Thanks for sharing!</small>
-                      <div className="d-flex flex-row align-items-center status"> <small>Like</small> <small>Reply</small> <small>Translate</small> <small>8 mins</small> </div>
-                    </div>
-                  </div>
-                  <div className="comment-input"> <input type="text" className="form-control" />
-                    <div className="fonts"> <i className="fa fa-camera"></i> </div>
-                  </div>
+                  <hr />
                 </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
     </div>
   )
 }
